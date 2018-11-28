d9c501b0bfac284f891b85718f6898bb7f67ec0e
Tomas Susanka <tsusanka@gmail.com>
2018-11-28 16:51:57 +0100

    python/norcow: get()

--- python/src/norcow.py
+++ python/src/norcow.py
@@ -6,11 +6,12 @@ NORCOW_SECTOR_SIZE = 64 * 1024
 NORCOW_MAGIC = b"NRCW"
 
 
-def align4(data):
-    if len(data) % 4 == 0:
-        return data
-    else:
-        return data + b"\x00" * (4 - len(data) % 4)
+def align4_int(i: int):
+    return 4 - i % 4
+
+
+def align4_data(data):
+    return data + b"\x00" * align4_int(len(data))
 
 
 class Norcow:
@@ -26,16 +27,39 @@ class Norcow:
         self.active_offset = len(NORCOW_MAGIC)
 
     def get(self, key: int) -> bytes:
-        raise NotImplementedError
+        return self._find_item(self.active_sector, key)
 
     def set(self, key: int, val: bytes) -> bool:
         if self.active_offset + 4 + len(val) > NORCOW_SECTOR_SIZE:
             self._compact()
-        data = pack("<HH", key, len(val)) + align4(val)
+        data = pack("<HH", key, len(val)) + align4_data(val)
         self.sectors[self.active_sector][
             self.active_offset : self.active_offset + len(data)
         ] = data
         self.active_offset += len(data)
+
+    def _find_item(self, sector: int, key: int) -> bytes:
+        offset = len(NORCOW_MAGIC)
+        value = False
+        key = key.to_bytes(2, "little")
+        while True:
+            try:
+                k, v = self._read_item(sector, offset)
+                if k == key:
+                    value = v
+            except ValueError as e:
+                break
+            offset = offset + 2 + 2 + len(v) + align4_int(len(v))
+        return value
+
+    def _read_item(self, sector: int, offset: int) -> (bytes, bytes):
+        key = self.sectors[self.active_sector][offset : offset + 2]
+        if key == b"\xff\xff":
+            raise ValueError("Norcow: no data on this offset")
+        length = self.sectors[self.active_sector][offset + 2 : offset + 4]
+        length = int.from_bytes(length, "little")
+        value = self.sectors[self.active_sector][offset + 4 : offset + 4 + length]
+        return key, value
 
     def _dump(self):
         return [bytes(x) for x in self.sectors]
---
+++ python/tests/test_norcow.py
@@ -0,0 +1,77 @@
+import sys
+from unittest import mock
+
+import pytest
+
+from src.norcow import Norcow
+
+from . import common
+
+sys.path.append("../src")
+
+
+class TestNorcow:
+    def test_nocrow_set(self):
+        n = Norcow()
+        n.init()
+        n.set(0x0001, b"123")
+        data = n._dump()[0][:256]
+        assert data[:4] == b"NRCW"
+        assert data[4:6] == b"\x01\x00"  # app + key
+        assert data[6:8] == b"\x03\x00"  # length
+        assert data[8:11] == b"123"  # data
+        assert common.all_ff_bytes(data[12:])
+
+        n.wipe()
+        n.set(0x0901, b"hello")
+        data = n._dump()[0][:256]
+        assert data[:4] == b"NRCW"
+        assert data[4:6] == b"\x01\x09"  # app + key
+        assert data[6:8] == b"\x05\x00"  # length
+        assert data[8:13] == b"hello"  # data
+        assert data[13:16] == b"\x00\x00\x00"  # alignment
+        assert common.all_ff_bytes(data[16:])
+
+        offset = 16
+        n.set(0x0102, b"world!")
+        data = n._dump()[0][:256]
+        assert data[offset : offset + 2] == b"\x02\x01"  # app + key
+        assert data[offset + 2 : offset + 4] == b"\x06\x00"  # length
+        assert data[offset + 4 : offset + 10] == b"world!"  # data
+        assert data[offset + 10 : offset + 12] == b"\x00\x00"  # alignment
+        assert common.all_ff_bytes(data[offset + 12 :])
+
+    def test_nocrow_read_item(self):
+        n = Norcow()
+        n.init()
+        n.set(0x0001, b"123")
+        n.set(0x0002, b"456")
+        n.set(0x0101, b"789")
+        key, value = n._read_item(0, 12)
+        assert key == b"\x02\x00"
+        assert value == b"456"
+        key, value = n._read_item(0, 20)
+        assert key == b"\x01\x01"
+        assert value == b"789"
+
+        with pytest.raises(ValueError) as e:
+            key, value = n._read_item(0, 200)
+        assert "no data" in str(e)
+
+    def test_nocrow_get_item(self):
+        n = Norcow()
+        n.init()
+        n.set(0x0001, b"123")
+        n.set(0x0002, b"456")
+        n.set(0x0101, b"789")
+        value = n.get(0x0001)
+        assert value == b"123"
+        value = n.get(0x0101)
+        assert value == b"789"
+        n.set(0x0101, b"hello")  # item changed
+        value = n.get(0x0101)
+        assert value == b"hello"
+        n.set(0x0002, b"world")
+        n.set(0x0002, b"earth")
+        value = n.get(0x0002)
+        assert value == b"earth"
