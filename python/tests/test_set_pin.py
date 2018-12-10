38a29e78182b2f302193cfe0f2719943b8ffd4ce
Tomas Susanka <tsusanka@gmail.com>
2018-12-10 13:44:37 +0100

    python: fix tests broken by hw salt

--- python/tests/test_set_pin.py
+++ python/tests/test_set_pin.py
@@ -13,23 +13,26 @@ class TestSetPin:
     def test_set_pin_success(self):
         with mock.patch.object(Prng, "random_buffer", common.mock_random_simple):
             s = Storage()
-            s.init()
+            hw_salt = b"\x00\x00\x00\x00\x00\x00"
+            s.init(hw_salt)
             s.set_pin(1)
             assert s.unlock(1)
 
             s = Storage()
-            s.init()
+            s.init(hw_salt)
             s.set_pin(229922)
             assert s.unlock(229922)
 
     def test_set_pin_failure(self):
         with mock.patch.object(Prng, "random_buffer", common.mock_random_simple):
             s = Storage()
-            s.init()
+            hw_salt = b"\x00\x00\x00\x00\x00\x00"
+            s.init(hw_salt)
             s.set_pin(1)
+            assert s.unlock(1)
             assert not s.unlock(1234)
 
             s = Storage()
-            s.init()
+            s.init(hw_salt)
             s.set_pin(229922)
             assert not s.unlock(1122992211)
